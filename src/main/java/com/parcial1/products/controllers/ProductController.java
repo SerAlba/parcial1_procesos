6b857a8b890d58fe97043cfcef72e2cdad0a75a8
SerAlba <seralba14@gmail.com>
2023-04-18 11:41:13 -0500

    Working on routes

--- src/main/java/com/parcial1/products/controllers/ProductController.java
+++ src/main/java/com/parcial1/products/controllers/ProductController.java
@@ -4,9 +4,7 @@ import com.parcial1.products.services.ProductServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.HashMap;
@@ -18,7 +16,7 @@ public class ProductController {
     private ProductServiceImp productServiceImp;
 
     @GetMapping(value = "/product/{id}")
-    public ResponseEntity getById (@PathVariable(name = "id") Long id) {
+    public ResponseEntity getById(@PathVariable(name = "id") Long id) {
         Map response = new HashMap();
 
         try {
@@ -30,7 +28,35 @@ public class ProductController {
             response.put("message", "Product not found");
             response.put("data", e.getMessage());
 
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping(value = "/listProducts")
+    public ResponseEntity getListProducts () {
+        Map response = new HashMap();
+
+        try {
+            response.put("message", "Products found");
+            response.put("data", productServiceImp.allProducts());
+
+            return new ResponseEntity(response, HttpStatus.OK);
+        } catch (Exception e) {
+            response.put("message", "There are no products");
+            response.put("data", e.getMessage());
+
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping(value = "/createProduct")
+    public ResponseEntity createProduct (@RequestBody Object idProduct) {
+        Map response = new HashMap();
+
+        System.out.print(idProduct);
+
+        response.put("data", idProduct);
+
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
