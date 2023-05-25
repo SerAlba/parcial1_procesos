081227b333c9d9968bacae6c35c9cd75a2e085f5
SerAlba <seralba14@gmail.com>
2023-05-25 10:04:34 -0500

    Register user

--- src/main/java/com/parcial1/products/services/UserServiceImp.java
+++ src/main/java/com/parcial1/products/services/UserServiceImp.java
@@ -3,9 +3,11 @@ package com.parcial1.products.services;
 import com.parcial1.products.models.User;
 import com.parcial1.products.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImp implements UserService {
     @Autowired
     private UserRepository userRepository;
